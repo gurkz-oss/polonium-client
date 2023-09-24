cbc9cef638dda344c01a41d601d9b50cb891bd82
UltimaxGG <amoghdhakate7@gmail.com>
2023-09-24 18:22:44 +0530

    Mixin-Base

---
+++ src/main/java/me/gurkz/client/polonium/client/PoloniumClient.java
@@ -0,0 +1,16 @@
+package me.gurkz.client.polonium.client;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PoloniumClient implements ClientModInitializer {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("Polonium");
+
+	@Override
+	public void onInitializeClient(ModContainer mod) {
+		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+	}
+}
